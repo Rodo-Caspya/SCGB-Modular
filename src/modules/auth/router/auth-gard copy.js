38e917b204cca32d39f3d5043f9db5fc62537440
Rodo-Caspya <87332892+Rodo-Caspya@users.noreply.github.com>
2022-04-11 14:26:05 -0500

    .

---
+++ src/modules/auth/router/auth-gard copy.js
@@ -0,0 +1,9 @@
+import { Store } from "../../../store";
+
+const isAuthenticatedGuard = async (to, from, next) => {
+  const { ok } = await Store.dispatch("authModule/checkAuthentication");
+  if (ok) next();
+  else next({ name: "login" });
+};
+
+export default isAuthenticatedGuard;
