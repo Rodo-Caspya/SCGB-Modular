aabcdc574adaa98cedf854b624b65b28dfec11c6
Rodo-Caspya <87332892+Rodo-Caspya@users.noreply.github.com>
2022-04-20 21:06:30 -0500

    .

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApi = axios.create({
-  baseURL: "https://back-end-modular.herokuapp.com/users",
+  baseURL: "http://localhost:9000/users",
 });
 
 export default authApi;
--- src/api/cowsApi.js
+++ src/api/cowsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const cowsApi = axios.create({
-  baseURL: "https://back-end-modular.herokuapp.com/vacas",
+  baseURL: "http://localhost:9000/vacas",
 });
 
 export default cowsApi;
