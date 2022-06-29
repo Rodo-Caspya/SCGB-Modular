a1f4f92864397a10fec6cc8e5836b22e8b1ac82f
Rodo-Caspya <87332892+Rodo-Caspya@users.noreply.github.com>
2022-06-29 18:08:14 -0500

    .

--- src/api/vaccinesApi.js
+++ src/api/vaccinesApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const vaccinesApi = axios.create({
-  // baseURL: "https://back-end-modular.herokuapp.com/vacas",
-  baseURL: "http://localhost:9000/vacunas",
+  baseURL: "https://back-end-modular.herokuapp.com/vacunas",
+  // baseURL: "http://localhost:9000/vacunas",
 });
 
 export default vaccinesApi;
