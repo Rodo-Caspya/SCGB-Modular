ba0b64806b907cb62129c91873e41562b062c0b8
Rodo-Caspya <87332892+Rodo-Caspya@users.noreply.github.com>
2022-06-29 22:52:46 -0500

    .

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const usersApi = axios.create({
-  // baseURL: "https://back-end-modular.herokuapp.com/users",
-  baseURL: "http://localhost:9000/users",
+  baseURL: "https://back-end-modular.herokuapp.com/users",
+  // baseURL: "http://localhost:9000/users",
 });
 
 export default usersApi;
